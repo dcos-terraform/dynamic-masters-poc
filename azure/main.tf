7064b6b6f03eb67221eff87c16c7b951b31e0e77
Weston Bassler <wbassler@mesosphere.com>
2018-12-12 14:37:17 -0500

    begin azure stuff

---
+++ azure/main.tf
@@ -0,0 +1,55 @@
+variable "dcos_install_mode" {
+  description = "specifies which type of command to execute. Options: install or upgrade"
+  default     = "install"
+}
+
+data "http" "whatismyip" {
+  url = "http://whatismyip.akamai.com/"
+}
+
+module "dcos" {
+  source  = "dcos-terraform/dcos/azurerm"
+  version = "~> 0.1"
+
+  cluster_name        = "dcos-test"
+  ssh_public_key_file = "~/.ssh/id_rsa.pub"
+  admin_ips           = ["${data.http.whatismyip.body}/32"]
+  location            = "West US"
+
+  num_masters        = "3"
+  num_private_agents = "2"
+  num_public_agents  = "1"
+
+  tags = {
+    owner = "soak-infra-team"
+    expiration = "2h"
+  }
+
+  dcos_version = "1.12.0"
+
+  dcos_variant              = "ee"
+  dcos_license_key_contents = "${file("./license.txt")}"
+
+  dcos_install_mode = "${var.dcos_install_mode}"
+
+  #DC/OS Config values that must be set
+  dcos_exhibitor_storage_backend        = "azure"
+  dcos_exhibitor_azure_account_name     = ""
+  dcos_exhibitor_azure_account_key      = ""
+  exhibitor_azure_prefix                = ""
+  dcos_master_discovery         = "master_http_loadbalancer"
+  dcos_exhibitor_address        = "${module.dcos.masters-internal-loadbalancer}"
+  dcos_num_masters               = "3"
+}
+
+output "masters-ips" {
+  value = "${module.dcos.masters-ips}"
+}
+
+output "cluster-address" {
+  value = "${module.dcos.masters-loadbalancer}"
+}
+
+output "public-agents-loadbalancer" {
+  value = "${module.dcos.public-agents-loadbalancer}"
+}
